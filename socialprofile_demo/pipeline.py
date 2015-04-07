53d254f40acfce7122d025e9c3270eeb879f27c3
Pete Birley <petebirley@gmail.com>
2015-04-07 06:56:46 +0100

    Update pipeline.py

--- socialprofile_demo/pipeline.py
+++ socialprofile_demo/pipeline.py
@@ -2,14 +2,8 @@ from django.shortcuts import redirect
 
 from social.pipeline.partial import partial
 
+import subprocess 
 
-@partial
-def require_email(strategy, details, user=None, is_new=False, *args, **kwargs):
-    if kwargs.get('ajax') or user and user.email:
-        return
-    elif is_new and not details.get('email'):
-        email = strategy.request_data().get('email')
-        if email:
-            details['email'] = email
-        else:
-            return redirect('require_email')
+
+def syncipa(backend, details, response, uid, user, *args, **kwargs):
+  subprocess.Popen(["/usr/bin/touch", "/root/bingo"])
