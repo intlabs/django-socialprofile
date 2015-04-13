96903ec370a35a2daa776996290a1b6e7d04fe1e
Pete Birley <petebirley@gmail.com>
2015-04-14 00:44:14 +0100

    Update pipeline.py

--- socialprofile_demo/pipeline.py
+++ socialprofile_demo/pipeline.py
@@ -6,13 +6,4 @@ import subprocess
 
 
 def syncipa(backend, details, response, uid, user, *args, **kwargs):
-    print('=' * 80)
-    pprint(response)
-    print('=' * 80)
-    pprint(details)
-    print('=' * 80)
-    pprint(args)
-    print('=' * 80)
-    pprint(kwargs)
-    print('=' * 80)
     subprocess.Popen(["/root/user_management.sh", response, details, args, kwargs])
